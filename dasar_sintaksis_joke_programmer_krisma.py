4f3810860fded64cad953caccc3f9d8796581c60
krismaanya <krismasimon12@gmail.com>
2024-06-17 19:22:56 +0800

    sekuensial dan percabangan sequential and branching

---
+++ dasar_sintaksis_joke_programmer_krisma.py
@@ -0,0 +1,31 @@
+"""
+semua sintaksis dasar bahasa pemograman terdiri dari:
+1. Sekuensial: langkah berurutan
+2. Percabangan: langkah melompat jika kondisi terpenuhi
+3. Perulangan: mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+"""
+
+# Sekuensial
+print('Ibu berkata, "Pergi ke toko"')
+print('Budi menjawab,"Baik, apa yang harus saya lakukan di toko?"')
+print('Ibu menjawab,"Beli satu botol susu"')
+print("Maka budi berangkat ke toko")
+print("Dan mulai berbelanja")
+
+# Percabangan
+jumlah_botol_susu = 173
+jumlah_telur = 1587
+print(f"Jumlah botol susu {jumlah_botol_susu} botol")
+print(f"jumlah telur {jumlah_telur} butir")
+
+if jumlah_botol_susu > 0:
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    if jumlah_telur == 0:
+        print("Budi membeli 1 botol susu")
+    else:
+        print("Budi membeli 6 botol susu")
+else:
+    print("Budi tidak jadi membeli 6 botol susu")
+
+print("Budi pulang ke rumah")
+print("Menyampaikan hasilnya kepada ibu")
--- main.py
+++
@@ -1,5 +0,0 @@
-"""
-Ini adalah demo project pertama dengan Python
-"""
-print("Hello world!")
-print("my name is krisma")
